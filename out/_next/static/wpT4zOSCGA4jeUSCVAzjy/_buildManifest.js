65bd097e52ba44be546cf5cc60b3e4c92952fd0d
kimbeomgyu <bg.kim@ejn.gg>
2022-12-04 20:31:10 +0900

    Deploy

--- out/_next/static/wpT4zOSCGA4jeUSCVAzjy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/708-c96e977259cba94c.js","static/chunks/pages/index-81a6faca92d019cc.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
