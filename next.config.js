850ddfa4ec06d57d598f16483e8b0efeb36d5861
kimbeomgyu <bg.kim@ejn.gg>
2022-12-04 20:01:49 +0900

    chore: 'basePath' setting in nextjs config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { unoptimized: true },
-  assetPrefix: "https://raw.githubusercontent.com/kimbeomgyu/q/gh-pages",
+  assetPrefix: ".",
+  basePath: "",
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
