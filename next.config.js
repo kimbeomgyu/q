34c8fcfe785ce7d7211fcdf2d8c015743069c00e
kimbeomgyu <bg.kim@ejn.gg>
2022-12-04 19:14:03 +0900

    chore: 'assetPrefix' setting in nextjs config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { unoptimized: true },
+  assetPrefix: ".",
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
